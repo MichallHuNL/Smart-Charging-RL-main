Correct price calculation

deep_reinforcement_learning/main.py
@@ -26,7 +26,9 @@ if __name__ == '__main__':
     params['p_max'] = P_c_max[0] / E_cap[0]
     params['p_max_grid'] = P_max_grid[0] / E_cap[0]
 
+    # Settings for plotting and checkpoints
     params['checkpoint_name'] = f"instance_N_{N}_id_{id}"
+    params['e_cap'] = E_cap[0]
 
     env = SmartChargingEnv(num_ports=num_agents, action_space_size=params.get('n_actions'), p_max=params.get('p_max'),
                            p_grid_max=params.get('p_max_grid'), leaving_soc=params.get('soc_req'), )
@@ -41,8 +43,9 @@ if __name__ == '__main__':
                                th.nn.Linear(128, n_actions + 1)) for _ in range(num_agents)]
     experiment = ActorCriticExperiment(params, models, env)
 
-    # experiment.load_checkpoint(90)
-    # experiment.test_instance(t_arr, t_dep, soc_int[0], prices)
+    experiment.load_checkpoint(230)
+    experiment.test_instance(t_arr, t_dep, soc_int[0], prices)
+    exit()
 
     # Re-executing this code-block picks up the experiment where you left off
     try:

deep_reinforcement_learning/smart_grid_environment/runner/Runner.py
@@ -29,7 +29,8 @@ class Runner:
         self.state = None
         self.time = 0
         self._next_step()
-        self.plot()
+        self.e_cap = params.get('e_cap', 50)
+        self.p_max = params.get('p_max', 0.8)
 
     def close(self):
         self.env.close()
@@ -177,6 +178,6 @@ class Runner:
         exists = observations[:, 3::4]
 
         make_plots(socs, actions, prices, exists, remaining_times, np.transpose(np.array(self.env.ends)),
-                   np.array(self.env.schedule), rewards)
+                   np.array(self.env.schedule), rewards, self.p_max, self.e_cap)
         self._next_step()
         # print("-------------------------      DONE       -------------------------")

deep_reinforcement_learning/smart_grid_environment/utils/plot.py
@@ -7,7 +7,6 @@ charging_reward_constant = 4
 non_full_ev_cost_constant = 15
 over_peak_load_constant = 5
 peak_load = 1.5
-p_max = 0.5
 
 
 def calculate_reward(soc, action, price, exists, end):
@@ -44,7 +43,7 @@ def get_action_if_ev(actions, exists):
     action_if_ev[exists != 1] = 0
     return action_if_ev
 
-def get_socs_when_leave(socs, actions, ends):
+def get_socs_when_leave(socs, actions, ends, p_max):
     socs_plus_leaves = socs
     for i in range(socs.shape[0]):
         for j in range(socs.shape[1]):
@@ -79,16 +78,15 @@ def find_non_zero_intervals(row):
 # remaining_times - numpy array of size (steps, num_agents)
 # ends - numpy array of size (steps, num_agents)
 # schedule - numpy array of size (steps, num_agents)
-def make_plots(socs, pre_filter_actions, prices, exists, remaining_times, ends, schedule, rewards):
+def make_plots(socs, pre_filter_actions, prices, exists, remaining_times, ends, schedule, rewards, p_max=0.2, E_cap=74):
     # actions_clipped = actions * p_max
     # actions_clipped = np.clip(actions_clipped, -socs, 1 - socs)
     # actions_clipped = np.clip(actions_clipped, -1, 0.5)
     actions = get_action_if_ev(pre_filter_actions, exists)
     # rewards, total_rewards = get_rewards(socs, actions, prices, exists, remaining_times, ends)
-    socs = get_socs_when_leave(socs, actions, ends)
+    socs = get_socs_when_leave(socs, actions, ends, p_max)
 
-    # TODO: fix this function, action is in (% of SOC)/P_MAX, while prices is in euro/kWh
-    total_cost = (np.transpose(prices) @ actions).sum()
+    total_cost = (np.transpose(prices) @ ((actions * p_max) * E_cap)).sum()
     print("total_cost", total_cost)
     # print(action_if_ev)
 