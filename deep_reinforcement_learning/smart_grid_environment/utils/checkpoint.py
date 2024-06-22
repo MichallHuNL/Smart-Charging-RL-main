cf992ad3d4b5238f96bee1f8c2b1726496f1c140
ekemmeren <e.a.kemmeren@student.tudelft.nl>
2024-06-22 21:56:23 +0200

    Pass None to load latest

--- deep_reinforcement_learning/smart_grid_environment/utils/checkpoint.py
+++ deep_reinforcement_learning/smart_grid_environment/utils/checkpoint.py
@@ -1,9 +1,11 @@
 import torch
 import os
+import re
 
 
 def save_checkpoint(epoch, models, optimizers, env_steps, losses, returns, lengths, checkpoint_name):
     os.makedirs("checkpoint", exist_ok=True)
+    os.makedirs(f"checkpoint/{checkpoint_name}", exist_ok=True)
     torch.save({
         'epoch': epoch,
         'models_state_dict': [model.state_dict() for model in models],
@@ -12,12 +14,18 @@ def save_checkpoint(epoch, models, optimizers, env_steps, losses, returns, lengt
         'losses': losses,
         'returns': returns,
         'lengths': lengths
-    }, f'checkpoint/{checkpoint_name}_epoch_{epoch}.pth')
+    }, f'checkpoint/{checkpoint_name}/epoch_{epoch}.pth')
     print(f'Checkpoint {epoch} saved', flush=True)
 
 
 def load_checkpoint(epoch, models, optimizers, checkpoint_name):
-    checkpoint = torch.load(f'checkpoint/{checkpoint_name}_epoch_{epoch}.pth')
+    if epoch is None:
+        files = os.listdir(f'checkpoint/{checkpoint_name}')
+
+        print(files)
+
+        epoch = max([int(re.search(r"[0-9]+", file).group()) for file in files])
+    checkpoint = torch.load(f'checkpoint/{checkpoint_name}/epoch_{epoch}.pth')
     [model.load_state_dict(checkpoint['models_state_dict'][idx]) for idx, model in enumerate(models)]
     [optimizer.load_state_dict(checkpoint['optimizers_state_dict'][idx]) for idx, optimizer in enumerate(optimizers)]
     epoch = checkpoint['epoch']
